c11c5b5b16cc5eb857163aeab190ff4c435b3fce
David Almeida <davidalmeidadrive@gmail.com>
2020-09-27 22:59:58 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Locadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
